e0b01defafad5551febae99f881a9a787cf145d1
faiaz hasan <98682289+faiaz5415@users.noreply.github.com>
2024-12-07 17:04:53 +0600

    first commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:dice_roller/dice_roller.dart';
+
+const startAlignment = Alignment.topCenter;
+const endAlignment = Alignment.bottomCenter;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(child: DiceRoller()),
+    );
+  }
+}
+
+//StyledText('Wellcome Home Again')
